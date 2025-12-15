396d4c6bb010eb9a63bf22ec6c051cd0502e4b40
Sundergupta <sunder1234gupta@gmail.com>
2025-12-15 11:47:28 +0530

    set up for Prerender

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import prerender from "prerender-node";   // <-- ⭐ ADD THIS
 
 dotenv.config();
 
@@ -17,6 +18,11 @@ const __dirname = path.dirname(__filename);
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// ⭐ PRERENDER MIDDLEWARE (must be BEFORE routes)
+app.use(
+    prerender.set("prerenderToken", "MkuCwme8QpJyYPjDqaXK")
+);
 
 // ⭐ Serve React build files
 app.use(express.static(path.join(__dirname, "../public")));
